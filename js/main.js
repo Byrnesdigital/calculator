bb535ddadc5e5b3803a3527af78310a0993a5c8c
Conor Byrnes <byrnesdigital@gmail.com>
2016-09-30 18:01:29 -0500

    Second commit, multiplication and subtraction working

--- js/main.js
+++ js/main.js
@@ -4,17 +4,21 @@ console.log(num1);
 var num2 = prompt("Segundo numero");
 console.log(num2);
 
-var operador = prompt("Operador");
+var operador = prompt("Operador: + - * or /");
 console.log(operador);
+
+var result;
 
 switch(operador){
 
-case "multiplicar":
+case "*":
     result = num1 + " por "+ num2 + " igual " + num1 * num2 + ".";
 break;
 
-case "resta":
+case "-":
     result = num1 + " menos "+ num2 + " igual " + num1 - num2 + ".";
 break;
 
 }
+
+console.log("Result: " + result);
