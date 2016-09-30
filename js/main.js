fc54d2ad1d6e7e800ccf3bdbd3425bcd673e14f0
Conor Byrnes <byrnesdigital@gmail.com>
2016-09-30 17:32:36 -0500

    First commit of calculator project with magda

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var num1 = prompt("Primer numero");
+console.log(num1);
+
+var num2 = prompt("Segundo numero");
+console.log(num2);
+
+var operador = prompt("Operador");
+console.log(operador);
+
+switch(operador){
+
+case "multiplicar":
+    result = num1 + " por "+ num2 + " igual " + num1 * num2 + ".";
+break;
+
+case "resta":
+    result = num1 + " menos "+ num2 + " igual " + num1 - num2 + ".";
+break;
+
+case "Division":
+    result = num1 + " Dividido por "+ num2 + " igual " + num1 * num2 + ".";
+break;
+
+case "Suma":
+    result = num1 + " suma "+ num2 + " igual " + num1 * num2 + ".";
+break;
+}
