abdf3705c3670133169591356a7f491392ba3360
Conor Byrnes <byrnesdigital@gmail.com>
2016-09-30 19:42:42 -0500

    I forgot if I changed anything and then I went to eat dinner so comitting for safety

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ var result;
 switch(operador){
 
 case "*":
-    result = num1 * num2;
+    result = +num1 * +num2;
 break;
 
 case "-":
-    result = num1 - num2;
+    result = +num1 - +num2;
 break;
 
 }
